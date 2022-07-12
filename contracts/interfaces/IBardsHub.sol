dedb0aa3a9acead892bda0b0765d395fb7274596
thebards <swordbookstory@gmail.com>
2022-07-12 14:29:45 +0800

    fix bugs

--- contracts/interfaces/IBardsHub.sol
+++ contracts/interfaces/IBardsHub.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.15;
 
 
 /**
- * @title ITheBardsHub
- * @author The Bards Protocol
+ * @title IBardsHub
+ * @author TheBards Protocol
  * 
  * @notice This is the interface for the TheBards contract, the main entry point for the TheBards Protocol.
  * You'll find all the events and external functions, as well as the reasoning behind them here.
